8a7b7897b63256600118d63ceab6034c366156e9
vivek13s09new <71691561+vivek13s09new@users.noreply.github.com>
2022-01-31 21:41:39 +0530

    Formatting spaces

--- Stack/largest-rectangle-in-histogram.cpp
+++ Stack/largest-rectangle-in-histogram.cpp
@@ -7,42 +7,42 @@ public:
         vector<int> RSE(n, 0); // next smaller element index on the right
         vector<int> LSE(n, 0); // next smaller element index on the left
         s.push(0);
-        LSE[0]=-1;
-        for (int i =1 ; i<n ; i++){
-            
+        LSE[0] =-1;
+        for (int i = 1 ; i < n ; i++){
             while(!s.empty() && heights[i] <= heights[s.top()])
                 s.pop();
             if(s.empty())
-                LSE[i]=-1;
+                LSE[i] = -1;
             else
-                LSE[i]=s.top();
+                LSE[i] = s.top();
             s.push(i);
         }
-        while(s.empty()== false)
+        while(s.empty() == false)
             s.pop();
         
         s.push(n-1);
-        RSE[n-1]=n;
-        for(int i=n-2; i>=0;i--){
-            while(!s.empty() && heights[i]<= heights[s.top()])
+        RSE[n-1] = n;
+        for(int i = n-2; i >=0; i--){
+            while(!s.empty() && heights[i] <= heights[s.top()])
                 s.pop();
             if(s.empty())
-                RSE[i]=n;
+                RSE[i] = n;
            else
-               RSE[i]=s.top();
+               RSE[i] = s.top();
         s.push(i);
         }
          
-        while(s.empty()== false)
+        while(s.empty() == false)
             s.pop();
         
-        int maxarea=0;
-        for(int i=0; i <n ; i++){
-            int width= RSE[i] -LSE[i]-1;
-            int area = heights[i]* width;
+        int maxarea = 0;
+        for(int i = 0; i <n ; i++){
+            int width = RSE[i] - LSE[i] - 1;
+            int area = heights[i] * width;
             if(area > maxarea){
               maxarea =  area;  
             }
         }
-        return maxarea ;}
+        return maxarea;
+    }
 };
